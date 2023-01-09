4a9e4ba354d76eff58dff12dd8494e8c131e8c3c
Prem8272 <116061099+Prem8272@users.noreply.github.com>
2023-01-09 17:17:00 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class typeconversion
+{
+public static void main(String[] ar)
+{
+char let='a';
+int ascii=let;
+System.out.println("Ascii value of "+let+" is "+ascii);
+char ex=(char)(ascii-32);
+int ef=ex;
+System.out.println("Ascii value of "+ex+" is "+ef);
+}
+}
