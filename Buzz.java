cd5a4c6293efb3c7de543ca35a69fae7f53c5ea5
Prem8272 <116061099+Prem8272@users.noreply.github.com>
2023-01-20 17:43:36 +0530

    Add files via upload

---
+++ Buzz.java
@@ -0,0 +1,23 @@
+class Buzz
+{
+	public static void main(String[] args)
+	{
+		int i=0;
+		for(i=0;i<=30;i++)
+		{
+			if(i%3==0)
+			{
+				System.out.println("Hello");
+			}
+			else if(i%5==0)
+			{
+				System.out.println("World");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
+		
---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+class Fibonacci
+{
+	public static void main(String[] args)
+	{
+		int a=1,b=1,c;
+		System.out.println(a);
+		System.out.println(b);
+		
+		for(int i=0;i<=10;i++)
+		{
+			
+			c=a+b;
+			System.out.println(c);
+			
+			a=b;
+			b=c;
+			
+		}
+	}
+}
+
+		
+		
